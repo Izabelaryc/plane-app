45b415337c6d882cd2b7541bde0fc4940b368b5b
Izabela Rycewicz <i.rycewicz@gmail.com>
2023-04-04 17:34:31 +0200

    Collecting user data

--- App.js
+++ App.js
@@ -4,10 +4,9 @@ import data from "./data";
 
 export default function App() {
   const [seatId, setSeatId] = React.useState(null);
-  const [premiumFilter, setPremiumFilter] = React.useState(false)
+  const [premiumFilter, setPremiumFilter] = React.useState(false);
 
   const seats = data.map((item) => {
-    console.log(premiumFilter, item.price)
     return (
       <Seat
         key={item.id}
@@ -29,42 +28,83 @@ export default function App() {
   function submit() {
     const selectedSeat = data.find((item) => item.id === seatId);
     setConfirmedSeat(selectedSeat);
-    setShouldBeOpen(prevState => !prevState)
   }
 
-  const [shouldBeOpen, setShouldBeOpen] = React.useState(false)
+  function closeDialog() {
+    setSeatId(null);
+    setConfirmedSeat(null);
+    setUserDataCollected(false)
+    setFormData({
+      firstName: "",
+      lastName: "",
+    })
+  }
+
+  function premium() {
+    setPremiumFilter((prevState) => !prevState);
+  }
+
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  function confirmName(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  const [userDataCollected, setUserDataCollected] = React.useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    if (formData.firstName !== "" && formData.lastName !== "") {
+      setUserDataCollected(true);
+    }
+  }
   
-  function closeDialog(){
-    setShouldBeOpen(false)
-    setSeatId(null)
-  }
-
-  function premium(){
-    setPremiumFilter(prevState => !prevState)
-  }
-
-  const[name, setName] = React.useState(null)
-
-  function confirmName(){
-    setName("Iza")
-  }
- 
+console.log(confirmedSeat, userDataCollected)
   return (
     <div className="main">
       <h1>Choose your seat</h1>
-      <button className="premium--btn" onClick={premium}>Show premium seats</button>
-      {confirmedSeat && !name && (
-        <dialog open={shouldBeOpen} className="dialog">
-          <input />
-          <button className="close--btn" onClick={confirmName}>OK</button>
-        </dialog>
-      )}
-      {confirmedSeat && name && (
-        <dialog open={shouldBeOpen} className="dialog">
-          <p>Your seat: {name} {confirmedSeat.seatNumber} <br></br> Cost: ${confirmedSeat.price}</p>
-          <button className="close--btn" onClick={closeDialog}>Go back</button>
-        </dialog>
-      )}
+      <button className="premium--btn" onClick={premium}>
+        Show premium seats
+      </button>
+      <dialog open={confirmedSeat && !userDataCollected} className="dialog">
+        <form onSubmit={handleSubmit}>
+            Provide your name:
+            <input
+              type="text"
+              placeholder="First name"
+              onChange={confirmName}
+              name="firstName"
+              value={formData.firstName}
+            />
+            <input
+              type="text"
+              placeholder="Last name"
+              onChange={confirmName}
+              name="lastName"
+              value={formData.lastName}
+            />
+          <button className="confirm--btn">Confirm</button>
+        </form>
+      </dialog>
+      <dialog open={confirmedSeat && userDataCollected} className="dialog">
+        <p>
+          Your seat: {confirmedSeat?.seatNumber} <br></br> First name:
+          {formData.firstName} <br></br> Last name: {formData.lastName}
+          <br></br> Cost: ${confirmedSeat?.price}
+        </p>
+        <button className="close--btn" onClick={closeDialog}>
+          Go back
+        </button>
+      </dialog>
       <div className="grid-container">{seats}</div>
 
       <button className="submit--btn" onClick={submit}>
