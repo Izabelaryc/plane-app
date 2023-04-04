c682bcee30c2822ac2b48e598d5985978484ddfa
Izabela Rycewicz <i.rycewicz@gmail.com>
2023-04-04 17:55:56 +0200

    defining state for steps

--- App.js
+++ App.js
@@ -2,10 +2,62 @@ import React from "react";
 import Seat from "./components/Seat";
 import data from "./data";
 
+const steps = {
+  plane: "plane",
+  userData: "userData",
+  summary: "summary"
+}
+
+const initialFormData = {
+  firstName: "",
+  lastName: "",
+}
 export default function App() {
   const [seatId, setSeatId] = React.useState(null);
   const [premiumFilter, setPremiumFilter] = React.useState(false);
+  const [formData, setFormData] = React.useState(initialFormData);
+  const [currentStep, setCurrentStep] = React.useState (steps.plane)
 
+  React.useEffect(() => {
+    if(currentStep === steps.plane){
+      setFormData(initialFormData)
+      setSeatId(null)
+    }
+  }, [currentStep])
+
+
+  function submit() {
+    if(seatId !== null){
+      setCurrentStep(steps.userData)
+    }
+  }
+
+  function closeDialog() {
+    setCurrentStep(steps.plane)
+  }
+
+  function premium() {
+    setPremiumFilter((prevState) => !prevState);
+  }
+
+  function confirmName(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    if (formData.firstName !== "" && formData.lastName !== "") {
+      setCurrentStep(steps.summary)
+    }
+  }
+
+  const confirmedSeat = data.find((item) => item.id === seatId);
   const seats = data.map((item) => {
     return (
       <Seat
@@ -23,79 +75,33 @@ export default function App() {
     );
   });
 
-  const [confirmedSeat, setConfirmedSeat] = React.useState(null);
-
-  function submit() {
-    const selectedSeat = data.find((item) => item.id === seatId);
-    setConfirmedSeat(selectedSeat);
-  }
-
-  function closeDialog() {
-    setSeatId(null);
-    setConfirmedSeat(null);
-    setUserDataCollected(false)
-    setFormData({
-      firstName: "",
-      lastName: "",
-    })
-  }
-
-  function premium() {
-    setPremiumFilter((prevState) => !prevState);
-  }
-
-  const [formData, setFormData] = React.useState({
-    firstName: "",
-    lastName: "",
-  });
-
-  function confirmName(event) {
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [event.target.name]: event.target.value,
-      };
-    });
-  }
-
-  const [userDataCollected, setUserDataCollected] = React.useState(false);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-    if (formData.firstName !== "" && formData.lastName !== "") {
-      setUserDataCollected(true);
-    }
-  }
-  
-console.log(confirmedSeat, userDataCollected)
   return (
     <div className="main">
       <h1>Choose your seat</h1>
       <button className="premium--btn" onClick={premium}>
         Show premium seats
       </button>
-      <dialog open={confirmedSeat && !userDataCollected} className="dialog">
+      <dialog open={currentStep === steps.userData} className="dialog">
         <form onSubmit={handleSubmit}>
-            Provide your name:
-            <input
-              type="text"
-              placeholder="First name"
-              onChange={confirmName}
-              name="firstName"
-              value={formData.firstName}
-            />
-            <input
-              type="text"
-              placeholder="Last name"
-              onChange={confirmName}
-              name="lastName"
-              value={formData.lastName}
-            />
+          Provide your name:
+          <input
+            type="text"
+            placeholder="First name"
+            onChange={confirmName}
+            name="firstName"
+            value={formData.firstName}
+          />
+          <input
+            type="text"
+            placeholder="Last name"
+            onChange={confirmName}
+            name="lastName"
+            value={formData.lastName}
+          />
           <button className="confirm--btn">Confirm</button>
         </form>
       </dialog>
-      <dialog open={confirmedSeat && userDataCollected} className="dialog">
+      <dialog open={currentStep === steps.summary} className="dialog">
         <p>
           Your seat: {confirmedSeat?.seatNumber} <br></br> First name:
           {formData.firstName} <br></br> Last name: {formData.lastName}
